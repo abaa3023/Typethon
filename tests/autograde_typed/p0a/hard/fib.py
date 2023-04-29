5d3ab70f5125ffb349cbadf8fe9e1eac7fa40c7e
abaa3023 <abaa3023@colorado.edu>
2023-04-29 20:19:39 +0000

    All cases in p0 and p0a passing except input related cases

--- tests/autograde_typed/p0a/hard/fib.py
+++ tests/autograde_typed/p0a/hard/fib.py
@@ -1,9 +1,9 @@
-n1 = 0
-n2 = 1
-count = 0
+n1:int = 0
+n2:int = 1
+count:int = 0
 while int(count != 10):
-  print(n1)
-  nth = n1 + n2
-  n1 = n2
-  n2 = nth
-  count = count + 1
+    print(n1)
+    nth:int = n1 + n2
+    n1 = n2
+    n2 = nth
+    count = count + 1
