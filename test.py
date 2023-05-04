4553a39ca3960074597fbd5ec2aef9ce8bcb428b
tgwalker93 <tgwalker93@gmail.com>
2023-05-04 05:32:34 +0000

    type checking for bool + int

---
+++ test.py
@@ -0,0 +1,26 @@
+x:list[int] = [1,True]
+print(x)
+
+
+# x:int = 1
+# y:bool = True
+# print(x + y)
+
+
+# x = 12
+# is_positive = 1
+# if int(x != 0):
+
+
+#     is_positive = 0
+
+# print(is_positive)
+
+# x = 1
+# print(x)
+
+
+# x:int = 1
+# y:bool = False
+
+# print(x+y)
